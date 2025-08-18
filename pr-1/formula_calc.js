4945f703cbd40e033a62f48612b7a5cab33ead6a
Jenish15133H <hjenis451@gmail.com>
2025-08-18 13:00:35 +0530

    pr-1

---
+++ pr-1/formula_calc.js
@@ -0,0 +1,20 @@
+var a = 5, b = 3, c = 2;
+
+
+var f1 = (a - b) * (a + b);
+console.log("a^2 - b^2 = " + f1);
+
+
+var f2 = (a*a) - (2*a*b) + (b*b);
+console.log("(a-b)^2 = " + f2);
+
+var f3 = (a*a) + (b*b) + (c*c) + 2*a*b + 2*a*c + 2*b*c;
+console.log("(a+b+c)^2 = " + f3);
+
+
+var f4 = (a*a) + (b*b) + (c*c) - 2*a*b - 2*a*c + 2*b*c;
+console.log("(a-b-c)^2 = " + f4);
+
+
+var f5 = (a*a*a) - (3*a*a*b) + (3*a*b*b) - (b*b*b);
+console.log("(a-b)^3 = " + f5);
--- pr-1/task-1.js
+++ pr-1/task-1.js
@@ -1,1 +1,32 @@
-alert("hello");
+
+// area of triangle
+
+
+var height = 25 ;
+var base = 10 ;
+
+var area = 0.5*base*height;
+
+console.log("area of triangle : " + area);
+
+// temperature 
+
+var celsius = 65 ;
+
+var Fahrenheit = (celsius*9/5) +32 ;
+
+console.log("65 celsius = " + Fahrenheit + " Fahrenheit") ;
+
+
+// Area of rectangle
+
+var length = 10;
+var width = 5;
+var rectArea = length * width;
+console.log("Area of Rectangle = " + rectArea);
+
+// Area of Circle
+
+var radius = 7;
+var circleArea = 3.14 * radius * radius;
+console.log("Area of Circle = " + circleArea);
