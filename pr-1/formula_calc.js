ea0e32f5c96d60fe07d4fc74c725ef387b94cfc8
Jenish15133H <hjenis451@gmail.com>
2025-08-18 17:39:10 +0530

    image change

--- pr-1/formula_calc.js
+++ pr-1/formula_calc.js
@@ -11,10 +11,8 @@ console.log("(a-b)^2 = " + f2);
 var f3 = (a*a) + (b*b) + (c*c) + 2*a*b + 2*a*c + 2*b*c;
 console.log("(a+b+c)^2 = " + f3);
 
-
 var f4 = (a*a) + (b*b) + (c*c) - 2*a*b - 2*a*c + 2*b*c;
 console.log("(a-b-c)^2 = " + f4);
-
 
 var f5 = (a*a*a) - (3*a*a*b) + (3*a*b*b) - (b*b*b);
 console.log("(a-b)^3 = " + f5);
