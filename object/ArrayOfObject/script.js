79e21dee72ab7bb9c4b569216d6eb29bd0c27330
JenishVasan <jenishvasan3@gmail.com>
2025-09-10 15:38:14 +0530

    quote-webpage

--- object/ArrayOfObject/script.js
+++ object/ArrayOfObject/script.js
@@ -1,22 +1,25 @@
+
+
+
 let student =[   {name:"jeni", position:"graphic" },
                 {name : "jenish" , position : "FUllStake " },
                 {name : "Bittu " , position : "java"}        
         ]
 
-let Box = document.querySelector(".box")
+// let Box = document.querySelector(".box")
 
 
 
 student.forEach(student =>{
 
-        let card = document.createElement("div")
-        card.classList.add("card")
 
-        card.innerHTML =`<h3> position : - ${student.name} </h3>
+        document.querySelector(".card").innerHTML +=`<h3> position : - ${student.name} </h3>
                         <p>position : ${student.position} </p>` ;   
 
-        Box.appendChild(card);
+        // Box.appendChild(card);
 
-})
+}
+
+)
 
 
---
+++ quote-webpage/script.js
@@ -0,0 +1,392 @@
+
+let data  =[  {
+
+  quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+
+  author: "Nelson Mandela",
+
+  imgurl: "./images/nelson-mandela.png",
+
+  birthPlace: "Mvezo, South Africa"
+
+ },
+ {
+
+  quote: "The way to get started is to quit talking and begin doing.",
+
+  author: "Walt Disney",
+
+  imgurl: "./images/walt-disney.png",
+
+  birthPlace: "Chicago, Illinois, USA"
+
+ },
+
+ {
+
+  quote: "Your time is limited, so don't waste it living someone else's life.",
+
+  author: "Steve Jobs",
+
+  imgurl: "./images/Steve-Jobs.png",
+
+  birthPlace: "San Francisco, California, USA"
+
+ },
+
+ {
+
+  quote: "If life were predictable it would cease to be life, and be without flavor.",
+
+  author: "Eleanor Roosevelt",
+
+  imgurl: "./images/Eleanor-Roosevelt.png",
+
+  birthPlace: "New York City, New York, USA"
+
+ },
+
+ {
+
+  quote: "If you look at what you have in life, you'll always have more.",
+
+  author: "Oprah Winfrey",
+
+  imgurl: "https://www.nicepng.com/png/full/229-2294493_oprah-winfrey-network-oprah-winfrey.png",
+
+  birthPlace: "Kosciusko, Mississippi, USA"
+
+ },
+
+ {
+
+  quote: "Life is what happens when you're busy making other plans.",
+
+  author: "John Lennon",
+
+  imgurl: "./images/John-Lennon.png",
+
+  birthPlace: "Liverpool, England"
+
+ },
+
+ {
+
+  quote: "Spread love everywhere you go. Let no one ever come to you without leaving happier.",
+
+  author: "Mother Teresa",
+
+  imgurl: "./images/mother-teresa.png",
+
+  birthPlace: "Skopje, North Macedonia"
+
+ },
+
+ {
+
+  quote: "When you reach the end of your rope, tie a knot in it and hang on.",
+
+  author: "Franklin D. Roosevelt",
+
+  imgurl: "./images/Franklin.png",
+
+  birthPlace: "Hyde Park, New York, USA"
+
+ },
+
+ {
+
+  quote: "Always remember that you are absolutely unique. Just like everyone else.",
+
+  author: "Margaret Mead",
+
+  imgurl: "./images/Margaret-Mead.png",
+
+  birthPlace: "Philadelphia, Pennsylvania, USA"
+
+ },
+
+ {
+
+  quote: "Don't judge each day by the harvest you reap but by the seeds that you plant.",
+
+  author: "Robert Louis Stevenson",
+
+  imgurl: "./images/Robert-Louis.png",
+
+  birthPlace: "Edinburgh, Scotland"
+
+ },
+
+ {
+
+  quote: "The future belongs to those who believe in the beauty of their dreams.",
+
+  author: "Eleanor Roosevelt",
+
+  imgurl: "./images/Eleanor-Roosevelt.png",
+
+  birthPlace: "New York City, New York, USA"
+
+ },
+
+ {
+
+  quote: "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
+
+  author: "Benjamin Franklin",
+
+  imgurl: "./images/Benjamin-Franklin.png",
+
+  birthPlace: "Boston, Massachusetts, USA"
+
+ },
+
+ {
+
+  quote: "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.",
+
+  author: "Helen Keller",
+
+  imgurl: "./images/helen-keller.png",
+
+  birthPlace: "Tuscumbia, Alabama, USA"
+
+ },
+
+ {
+
+  quote: "It is during our darkest moments that we must focus to see the light.",
+
+  author: "Aristotle",
+
+  imgurl: "./images/Aristotle.png",
+
+  birthPlace: "Stagira, Greece"
+
+ },
+
+ {
+
+  quote: "Whoever is happy will make others happy too.",
+
+  author: "Anne Frank",
+
+  imgurl: "./images/Anne-Frank.png",
+
+  birthPlace: "Frankfurt, Germany"
+
+ },
+
+ {
+
+  quote: "Do not go where the path may lead, go instead where there is no path and leave a trail.",
+
+  author: "Ralph Waldo Emerson",
+
+  imgurl: "./images/Ralph Waldo.png",
+
+  birthPlace: "Boston, Massachusetts, USA"
+
+ },
+
+ {
+
+  quote: "You will face many defeats in life, but never let yourself be defeated.",
+
+  author: "Maya Angelou",
+
+  imgurl: "./images/Maya-Angelou.png",
+
+  birthPlace: "St. Louis, Missouri, USA"
+
+ },
+
+ {
+
+  quote: "In the end, it's not the years in your life that count. It's the life in your years.",
+
+  author: "Abraham Lincoln",
+
+  imgurl: "./images/Abraham-Lincoln.png",
+
+  birthPlace: "Hodgenville, Kentucky, USA"
+
+ },
+
+ {
+
+  quote: "Never let the fear of striking out keep you from playing the game.",
+
+  author: "Babe Ruth",
+
+  imgurl: "./images/Babe-Ruth.png",
+
+  birthPlace: "Baltimore, Maryland, USA"
+
+ },
+
+ {
+
+  quote: "Life is either a daring adventure or nothing at all.",
+
+  author: "Helen Keller",
+
+  imgurl: "./images/helen-keller.png",
+
+  birthPlace: "Tuscumbia, Alabama, USA"
+
+ },
+
+ {
+
+  quote: "Many of life's failures are people who did not realize how close they were to success when they gave up.",
+
+  author: "Thomas A. Edison",
+
+  imgurl: "./images/Thomas-Edison.png",
+
+  birthPlace: "Milan, Ohio, USA"
+
+ },
+
+ {
+
+  quote: "You have within you right now, everything you need to deal with whatever the world can throw at you.",
+
+  author: "Brian Tracy",
+
+  imgurl: "./images/Brian-Tracy.png",
+
+  birthPlace: "Charlottetown, Canada"
+
+ },
+
+ {
+
+  quote: "Believe you can and you're halfway there.",
+
+  author: "Theodore Roosevelt",
+
+  imgurl: "./images/Theodore-Roosevelt.png",
+
+  birthPlace: "New York City, New York, USA"
+
+ },
+
+ {
+
+  quote: "Act as if what you do makes a difference. It does.",
+
+  author: "William James",
+
+  imgurl: "./images/William-James.png",
+
+  birthPlace: "New York City, New York, USA"
+
+ },
+
+ {
+
+  quote: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+
+  author: "Winston Churchill",
+
+  imgurl: "./images/Winston-Churchill.png",
+
+  birthPlace: "Woodstock, England"
+
+ },
+
+ {
+
+  quote: "You are never too old to set another goal or to dream a new dream.",
+
+  author: "C.S. Lewis",
+
+  imgurl: "./images/Lewis.png",
+
+  birthPlace: "Belfast, Ireland"
+
+ },
+
+ {
+
+  quote: "At any given moment you have the power to say: this is not how the story is going to end.",
+
+  author: "Christine Mason Miller",
+
+  imgurl: "./images/Christine-Miller.png",
+
+  birthPlace: "Unknown"
+
+ },
+
+ {
+
+  quote: "You don't have to be great to start, but you have to start to be great.",
+
+  author: "Zig Ziglar",
+
+  imgurl: "./images/Zig-Ziglar.png",
+
+  birthPlace: "Coffee County, Alabama, USA"
+
+ },
+
+ {
+
+  quote: "I have not failed. I've just found 10,000 ways that won't work.",
+
+  author: "Thomas A. Edison",
+
+  imgurl: "./images/Thomas-Edison.png",
+
+  birthPlace: "Milan, Ohio, USA"
+
+ },
+
+ {
+
+  quote: "Don't watch the clock; do what it does. Keep going.",
+
+  author: "Sam Levenson",
+
+  imgurl: "./images/Sam-Levenson.png",
+
+  birthPlace: "New York City, New York, USA"
+
+ }
+];
+
+
+
+let str ="";
+
+
+data.forEach((obj)=>{
+
+
+str +=` <div class="col-12 d-flex jc-center">
+                        <div class="col-12 d-flex jc-center">
+                            <div class="col-3" >
+                                <div id="auther-img">
+                                    <img src="${obj.imgurl}" alt="img">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-8 quote-box ">
+                            <div class= "text-center">
+                                <p id="quote col-12"><span >"</span>${obj.quote}<span>"</span></p>
+            
+                            </div>
+                            <div class="text-end mt-10">
+                                ~${obj.author}
+                            </div>
+                        </div>
+                    </div>`
+
+
+})
+
+document.querySelector(".quote-container").innerHTML = str 
